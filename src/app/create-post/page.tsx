56c78a6c8657c1fe7288885489f4f5e804274b35
Nandra Mihnea <nandra.mihnea@gmail.com>
2024-06-05 18:00:48 +0300

    final

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,15 +1,11 @@
-import { createPost } from "@/actions/actions";
+import Form from "@/components/form";
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default async function Page() {
   return (
     <main className="text-center pt-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">Create Post</h1>
-      <form action={createPost} className="flex flex-col max-w-[400px] mx-auto gap-2 my-10">
-        <input type="text" name="title" placeholder="Title for new post" required className="border rounded px-3 py-1" />
-        <textarea name="body" placeholder="Body content for new post" className="border rounded px-3 py-2" rows={6} required />
-        <button className="h-10 bg-blue-500 px-5 rounded text-white">Submit</button>
-      </form>
+      <Form />
       <LogoutLink>Log out</LogoutLink>
     </main>
   );
